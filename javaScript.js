8021c3ba379d80160c06a2b89d1495838f8e152d
cristobaldp <cdelgadopenaranda@gmail.com>
2024-11-26 11:06:30 +0100

    añadidos css y js

---
+++ javaScript.js
@@ -0,0 +1,17 @@
+//Codigo para un boton menú con enlaces con los que acceder a partes especificas de la información
+const botonMenu = document.getElementById('botonMenu');
+const menu = document.getElementById('menu');
+const enlacesMenu = document.querySelectorAll('#menu a');
+
+botonMenu.addEventListener('click', () => {
+  menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+});
+
+enlacesMenu.forEach(enlace => {
+  enlace.addEventListener('click', (evento) => {
+    const idEnlace = enlace.getAttribute('href').substring(1); // Obtiene el ID del destino
+    const EnlaceDestino = document.getElementById(idEnlace);
+      EnlaceDestino.scrollIntoView({ behavior: 'smooth' }); // Desplaza suavemente al elemento
+    
+  });
+});
