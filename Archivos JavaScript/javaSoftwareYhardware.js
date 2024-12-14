060b3775ed8a64f0776f89295d615a65fb5cb034
rubxx28 <benrupro28@gmail.com>
2024-12-14 14:31:01 +0100

    mis preguntas y el java de ellas

---
+++ Archivos JavaScript/javaSoftwareYhardware.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('testForm');
+const resultParagraph = document.getElementById('result');
+
+// Personaliza el número de preguntas y las respuestas correctas
+const totalQuestions = 20;
+const correctAnswers = ['RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta','RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta','RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta','RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta','RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta'];
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let correctCount = 0;
+    let incorrectAnswers = [];
+
+    for (let i = 1; i <= totalQuestions; i++) {
+        const selectedAnswer = document.querySelector(`input[name=tipoRespuesta${i}]:checked`).value;
+        if (selectedAnswer === correctAnswers[i - 1]) {
+            correctCount++;
+        } else {
+            incorrectAnswers.push(i);
+        }
+    }
+
+    const grade = (correctCount / totalQuestions) * 100;
+
+    resultParagraph.textContent = `Tu nota es: ${grade}%`;
+
+    if (incorrectAnswers.length > 0) {
+        resultParagraph.textContent += `\nPreguntas falladas: ${incorrectAnswers.join(', ')}`;
+    }
+});
+
