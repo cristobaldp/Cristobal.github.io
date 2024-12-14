6f8845996181e40455507a4dd84a81f98900fd60
rubxx28 <benrupro28@gmail.com>
2024-12-14 14:48:26 +0100

    modelos y metodologias, arreglo en las pregumtas y java que les hace funcionar

--- Archivos JavaScript/javaModelosMetodologias.js
+++ Archivos JavaScript/javaModelosMetodologias.js
@@ -18,3 +18,33 @@ if (imagen) {
 } else {
   console.error("No se encontró la imagen con el ID 'imagen-zoom'");
 }
+const form = document.getElementById('testForm');
+const resultParagraph = document.getElementById('result');
+
+// Personaliza el número de preguntas y las respuestas correctas
+const totalQuestions = 16;
+const correctAnswers = ['RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta','RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta','RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta','RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta', 'RespuestaCorrecta'];
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let correctCount = 0;
+    let incorrectAnswers = [];
+
+    for (let i = 1; i <= totalQuestions; i++) {
+        const selectedAnswer = document.querySelector(`input[name=tipoRespuesta${i}]:checked`).value;
+        if (selectedAnswer === correctAnswers[i - 1]) {
+            correctCount++;
+        } else {
+            incorrectAnswers.push(i);
+        }
+    }
+
+    const grade = (correctCount / totalQuestions) * 100;
+
+    resultParagraph.textContent = `Tu nota es: ${grade}%`;
+
+    if (incorrectAnswers.length > 0) {
+        resultParagraph.textContent += `\nPreguntas falladas: ${incorrectAnswers.join(', ')}`;
+    }
+});
