f7f7de9789f32fcd88853b5d8ec46788df33c519
rubxx28 <benrupro28@gmail.com>
2024-12-15 21:54:44 +0100

    eliminados archivos sobrantes

--- Archivos JavaScript/javaModelosMetodologias.js
+++ Archivos JavaScript/javaModelosMetodologias.js
@@ -1,23 +1,3 @@
-// Selecciona la imagen
-const imagen = document.getElementById('imagen-zoom');
-var amplicion = 5
-
-// Verifica si la imagen fue encontrada
-if (imagen) {
-  // Agrega un evento de clic
-  imagen.addEventListener('click', () => {
-    console.log("Imagen clickeada");
-
-    // Si la imagen está ampliada, vuelve a su tamaño original
-    if (imagen.style.transform === 'scale(1.'+ amplicion + ')') {
-      imagen.style.transform = 'scale(1)';
-    } else {
-      imagen.style.transform = 'scale(1.'+ amplicion + ')';
-    }
-  });
-} else {
-  console.error("No se encontró la imagen con el ID 'imagen-zoom'");
-}
 const form = document.getElementById('testForm');
 const resultParagraph = document.getElementById('result');
 
